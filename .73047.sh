1e9fcd000c105ee6cad767508d80a401f1ad34c1
handy036 <hanxiaodong036@163.com>
2012-03-03 16:03:00 +0800

    add dbtable and models of acuser

---
+++ .73047.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+export PATH=:"/Applications/Zend Studio.app/Contents/Resources/Java/plugins/com.zend.php.debug.debugger.macosx_5.3.18.v20111214/resources/php53":/usr/bin:/bin:/usr/sbin:/sbin
+export DYLD_LIBRARY_PATH="/Applications/Zend Studio.app/Contents/Resources/Java/plugins/com.zend.php.debug.debugger.macosx_5.3.18.v20111214/resources/lib"
+"/Users/hanxiaodong036/Zend/workspaces/DefaultWorkspace/.metadata/.plugins/org.zend.php.framework.resource/resources/ZendFramework-1/bin/zf.sh" create db-table ACUser
---
+++ application/models/DbTable/Acuser.php
@@ -0,0 +1,10 @@
+<?php
+
+class Application_Model_DbTable_Acuser extends Zend_Db_Table_Abstract
+{
+
+    protected $_name = 'acuser';
+
+
+}
+
