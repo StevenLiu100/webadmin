0e4ae755a83ac08443539c6f2f951966d435c15c
hantao <hantao@icbc-dccbj7557>
2012-03-06 22:56:08 +0800

    add the deleted files

---
+++ application/models/Unit.php
@@ -0,0 +1,102 @@
+<?php
+
+class Application_Model_Unit
+{
+	protected $_unitid;
+	protected $_unitname;
+	protected $_parentid;
+	protected $_unitorder;
+	/**
+	 * @return the $_unitid
+	 */
+	public function getUnitid() {
+		return $this->_unitid;
+	}
+
+	/**
+	 * @return the $_unitname
+	 */
+	public function getUnitname() {
+		return $this->_unitname;
+	}
+
+	/**
+	 * @return the $_parentid
+	 */
+	public function getParentid() {
+		return $this->_parentid;
+	}
+
+	/**
+	 * @return the $_unitorder
+	 */
+	public function getUnitorder() {
+		return $this->_unitorder;
+	}
+
+	/**
+	 * @param field_type $_unitid
+	 */
+	public function setUnitid($_unitid) {
+		$this->_unitid = $_unitid;
+	}
+
+	/**
+	 * @param field_type $_unitname
+	 */
+	public function setUnitname($_unitname) {
+		$this->_unitname = $_unitname;
+	}
+
+	/**
+	 * @param field_type $_parentid
+	 */
+	public function setParentid($_parentid) {
+		$this->_parentid = $_parentid;
+	}
+
+	/**
+	 * @param field_type $_unitorder
+	 */
+	public function setUnitorder($_unitorder) {
+		$this->_unitorder = $_unitorder;
+	}
+
+	public function __construct(array $options = null)
+	{
+		if (is_array($options)) {
+			$this->setOptions($options);
+		}
+	}
+	
+	public function __set($name, $value)
+	{
+		$method = 'set' . $name;
+		if (('mapper' == $name) || !method_exists($this, $method)) {
+			throw new Exception('Invalid unit property');
+		}
+		$this->$method($value);
+	}
+	
+	public function __get($name)
+	{
+		$method = 'get' . $name;
+		if (('mapper' == $name) || !method_exists($this, $method)) {
+			throw new Exception('Invalid unit property');
+		}
+		return $this->$method();
+	}
+	
+	public function setOptions(array $options)
+	{
+		$methods = get_class_methods($this);
+		foreach ($options as $key => $value) {
+			$method = 'set' . ucfirst($key);
+			if (in_array($method, $methods)) {
+				$this->$method($value);
+			}
+		}
+		return $this;
+	}
+}
+
---
+++ application/models/UnitMapper.php
@@ -0,0 +1,104 @@
+<?php
+
+class Application_Model_UnitMapper
+{
+	protected $_dbTable;
+	
+	public function setDbTable($dbTable)
+	{
+		if (is_string($dbTable)) {
+			$dbTable = new $dbTable();
+		}
+		if (!$dbTable instanceof Zend_Db_Table_Abstract) {
+			throw new Exception('Invalid table data gateway provided');
+		}
+		$this->_dbTable = $dbTable;
+		return $this;
+	}
+	
+	public function getDbTable()
+	{
+		if (null === $this->_dbTable) {
+			$this->setDbTable('Application_Model_Unit');
+		}
+		return $this->_dbTable;
+	}
+
+	public function save(Application_Model_Unit $unit)
+	{
+		$id = $unit->getUnitid();
+			
+		$data = array(
+				'unitname' => $unit->getUnitname(),
+				'parentid'=> $unit->getParentid(),
+				'unitorder'=>$unit->getUnitorder(),
+		);
+	
+		if ($id == 0) {
+			unset($data['unitid']);
+			$this->getDbTable()->insert($data);
+		} else {
+			$this->getDbTable()->update($data, array('unitid = ?' => $id));
+		}
+	}
+	
+	public function fetchAll()
+	{
+		$resultSet = $this->getDbTable()->fetchAll();
+		$units   = array();
+		foreach ($resultSet as $row) {
+			$unit = new Application_Model_Unit();
+			$unit->setUnitid($row->unitid);
+			$unit->setUnitname($row->unitname);
+			$unit->setParentid($row->parentid);
+			$unit->setUnitorder($row->unitorder);
+
+			$units[] = $unit;
+		}
+		return $units;
+	}
+	
+	public function delete($unitid)
+	{
+		$this->getDbTable()->delete(array('unitid = ?' => $unitid));
+	}
+	
+	public function unitSearch($parentid, $key)
+	{
+		$resultSet = $this->getDbTable()->fetchAll(
+				$this->getDbTable()->select()
+				->where('unitname like ? and parentid = ?', '%'.$key.'%', $parentid)
+				->order('unitorder')
+		);
+		
+		$units   = array();
+		foreach ($resultSet as $row) {
+			$unit = new Application_Model_Unit();
+			$unit->setUnitid($row->unitid);
+			$unit->setUnitname($row->unitname);
+			$unit->setParentid($row->parentid);
+			$unit->setUnitorder($row->unitorder);
+		
+			$units[] = $unit;
+		}
+		return $units;
+	}
+	
+	public function parentunit()
+	{
+		$resultSet = $this->getDbTable()->fetchAll(
+				$this->getDbTable()->select()
+				->where('parentid = ?', 0)
+				->order('unitorder')
+		);
+		
+		$units = array();
+		foreach ($resultSet as $row) {
+			$unit = $row->unitid.'=>'.$row->unitname;
+
+			$units[] = $unit;
+		}
+		return $units;
+	}
+}
+
