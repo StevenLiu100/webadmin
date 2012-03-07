4bb0d11f692e2c4b72d9f9727fde58a04f7ebc2d
StevenLiu100 <StevenLiu100@163.com>
2012-03-07 08:33:10 +0800

    加入程序错误提示，和默认上海时间

--- public/index.php
+++ public/index.php
@@ -1,4 +1,7 @@
 <?php
+error_reporting(E_ALL|E_STRICT);
+ini_set('display_errors', 1);
+date_default_timezone_set('Asia/Shanghai');
 
 // Define path to application directory
 defined('APPLICATION_PATH')
