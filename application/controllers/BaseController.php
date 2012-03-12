98125825292e400c13333240b4e85b847f702d82
Steven Liu <stevenliu100@163.com>
2012-03-12 22:45:20 +0800

    完善了sitmap功能，即使配置文件没有对应的uri也可以找到上一级目录

--- application/controllers/BaseController.php
+++ application/controllers/BaseController.php
@@ -1,17 +1,38 @@
-<?php
 
+<?php
 class BaseController extends Zend_Controller_Action
 {
-
-    public function init()
-    {
-        $uri = $this->_request->getPathInfo();
+    /**
+     * 根据当前Uri来确定sitmap,如果没有在配置文件中找到，则进行截取，知道找到
+     */
+    private function inituri4breadcrumb(){
+    	$uri = $this->_request->getPathInfo();
     	$activeNav = $this->view->navigation()->findByUri($uri);
-    	if($activeNav!=null){
-    	  $activeNav-> active = true;
-    	  $activeNav->setClass("active");
+    	
+    	if($activeNav==null){
+    		do{
+    			$endindex=strrpos($uri,"/",0);
+    			if($endindex!=false)
+    			{
+    				$uri=substr($uri,0,$endindex);
+    				print("this is uri:".$uri);
+    			}
+    			else
+    			{
+    				$uri="/";
+    			}
+    			$activeNav = $this->view->navigation()->findByUri($uri);
+    		}
+    		while($activeNav==null);
     	}
+    	$activeNav-> active = true;
+    	$activeNav->setClass("active");
     }
+	public function init()
+    {
+    	$this->inituri4breadcrumb();
+    }
+    
 
     public function indexAction()
     {
