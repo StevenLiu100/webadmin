e0964067a47585a2209c8842d43fb388869ee040
hantao <hantao@icbc-dccbj7557>
2012-03-17 21:36:04 +0800

    unit 管理

--- application/forms/Unitadd.php
+++ application/forms/Unitadd.php
@@ -8,7 +8,14 @@ class Application_Form_Unitadd extends Zend_Form
     	$mapper = new Application_Model_UnitMapper();
         /* Form Elements & Other Definitions Here ... */
     	$this->setMethod('post');
-    	$this->addElement('select', 'parentid', array(
+    	$validator_isEmpty = new Zend_Validate_NotEmpty();
+    	$validator_isEmpty->setMessage('这里不能为空',Zend_Validate_NotEmpty::IS_EMPTY);
+    	 
+    	$validator_stringlength_2_100 =new Zend_Validate_StringLength(array('min' => 2,'max' => 100));
+    	$validator_stringlength_2_100->setMessage('少于 %min% 个字符',Zend_Validate_StringLength::TOO_SHORT);
+    	$validator_stringlength_2_100->setMessage('多于 %max% 个字符',Zend_Validate_StringLength::TOO_LONG);
+    	
+    	$this->addElement('select', 'firstparentid', array(
     			'label'      => '一级单位选择',
     			'required'   => true,
     			'filters'    => array('StringTrim'),
@@ -19,7 +26,7 @@ class Application_Form_Unitadd extends Zend_Form
     			'multioptions'   => $mapper->parentunit()
     	));
     	
-    	$this->addElement('select', 'subparentid', array(
+    	$this->addElement('select', 'parentid', array(
     			'label'      => '二级单位选择',
     			'required'   => true,
     			'filters'    => array('StringTrim'),
@@ -30,18 +37,12 @@ class Application_Form_Unitadd extends Zend_Form
     			'multioptions'   => $mapper->parentunit()
     	));
     	
-    	$this->addElement('text', 'unitnameforadd', array(
-    			'label'      => '单位名称',
-    			'required'   => true,
-    			'filters'    => array('StringTrim'),
-    			'validators' => array(
-    					array('validator' => 'StringLength', 'options' => array(2, 100))
-    			)
-    	));
-    	
-//     	$unitorder = new Zend_Form_Element_Hidden('unitorder');
-//     	$unitorder->addFilter('Int');
-//     	$this->addElement($unitorder);
+    	$unitname = new Zend_Form_Element_Text('unitname');
+    	$unitname->setLabel('单位名称')
+    	->setRequired(true)
+    	->setFilters(array('StringTrim'))
+    	->addValidator($validator_stringlength_2_100);
+    	$this->addElement($unitname);
     	
     	// Add a submit button
     	$this->addElement('submit', 'submit', array(
@@ -51,6 +52,9 @@ class Application_Form_Unitadd extends Zend_Form
     	$unitid = new Zend_Form_Element_Hidden('unitid');
     	$unitid->addFilter('Int');
     	$this->addElement($unitid);
+    	$unitorder = new Zend_Form_Element_Hidden('unitorder');
+    	$unitorder->addFilter('Int');
+    	$this->addElement($unitorder);
     }
 
 
--- application/forms/Unitupd.php
+++ application/forms/Unitupd.php
@@ -6,10 +6,13 @@ class Application_Form_Unitupd extends Zend_Form
     public function init()
     {
         $this->setMethod('post');
-    	$unitid = new Zend_Form_Element_Hidden('unitid');
-    	$unitid->addFilter('Int');
-    	$this->addElement($unitid);
-    	 
+        $validator_isEmpty = new Zend_Validate_NotEmpty();
+        $validator_isEmpty->setMessage('这里不能为空',Zend_Validate_NotEmpty::IS_EMPTY);
+        
+        $validator_stringlength_2_100 = new Zend_Validate_StringLength(array('min' => 2,'max' => 100));
+        $validator_stringlength_2_100->setMessage('少于 %min% 个字符',Zend_Validate_StringLength::TOO_SHORT);
+        $validator_stringlength_2_100->setMessage('多于 %max% 个字符',Zend_Validate_StringLength::TOO_LONG);
+
     	$this->addElement('text', 'parentid', array(
     			'label'      => '归属大单位',
     			'required'   => true,
@@ -19,25 +22,27 @@ class Application_Form_Unitupd extends Zend_Form
     					array('validator' => 'StringLength', 'options' => array(0, 100))
     			),
     	));
-    	 
-    	$this->addElement('text', 'unitname', array(
-    			'label'      => '新单位名称',
-    			'required'   => true,
-    			'filters'    => array('StringTrim'),
-    			'validators' => array(
-    					array('validator' => 'StringLength', 'options' => array(2, 100))
-    			)
-    	));
 
-    	$unitorder = new Zend_Form_Element_Hidden('unitorder');
-    	$unitorder->addFilter('Int');
-    	$this->addElement($unitorder);
-    	
+    	$unitname = new Zend_Form_Element_Text('unitname');
+    	$unitname->setLabel('新单位名称')
+    	->setRequired(true)
+    	->setFilters(array('StringTrim'))
+    	->addValidator($validator_stringlength_2_100);
+    	$this->addElement($unitname);
+    	   	
     	// Add a submit button
     	$this->addElement('submit', 'submit', array(
     			'ignore'   => true,
     			'label'    => '保存',
     	));
+    	
+    	$unitid = new Zend_Form_Element_Hidden('unitid');
+    	$unitid->addFilter('Int');
+    	$this->addElement($unitid);
+    	
+    	$unitorder = new Zend_Form_Element_Hidden('unitorder');
+    	$unitorder->addFilter('Int');
+    	$this->addElement($unitorder);
     }
 
 }
